69e8e7c116228d406477fd6a1bca480c829cb2f8
Júlio Santos <jcarsan1000@gmail.com>
2018-01-28 20:41:59 -0200

    Update quicksort.py

--- Ordenação/quicksort.py
+++ Ordenação/quicksort.py
@@ -1,19 +1,19 @@
 def partition(A,p,r):
-	  x = A[r]
-	  i = p - 1
-	  for j in range(p,r):
-		    if(A[j] <= x):
-			      i = i + 1
-			      aux = A[i]
-			      A[i] = A[j]
-			      A[j] = aux
-	  aux = A[i+1]
-	  A[i+1] = A[r]
-	  A[r] = aux
-	  return i + 1
+    x = A[r]
+    i = p - 1
+    for j in range(p,r):
+        if(A[j] <= x):
+	    i = i + 1
+	    aux = A[i]
+	    A[i] = A[j]
+	    A[j] = aux
+    aux = A[i+1]
+    A[i+1] = A[r]
+    A[r] = aux
+    return i + 1
     
 def quick_sort(A,p,r):
 	  if(p < r):
-		    q = partition(A,p,r)
-		    quick_sort(A,p,q-1)
-		    quick_sort(A,q+1,r)
+	      q = partition(A,p,r)
+	      quick_sort(A,p,q-1)
+	      quick_sort(A,q+1,r)
